aa4fd576f77344eb4480c76141572373b5f79014
Axel <axlds98@gmail.com>
2018-12-06 22:07:41 +0100

    acceloromettre test

---
+++ apli-telephone/src/js/components/Acceleration.js
@@ -0,0 +1,18 @@
+import Root from "../components/Root";
+
+class Acceleration{
+    constructor(){
+        this.bindEvents();
+        this.init();
+    }
+
+    init(){
+    }
+
+    bindEvents(){
+        // Bind every events here
+    }
+
+}
+
+export default MainRedirect;
--- apli-telephone/src/js/main.js
+++ apli-telephone/src/js/main.js
@@ -7,6 +7,11 @@ const App = {
 
     init(){
         document.addEventListener('DOMContentLoaded', this.ready.bind(this), false);
+        if(window.DeviceMotionEvent) {
+            window.addEventListener("devicemotion", process, false);
+          } else {
+            // Le navigateur ne supporte pas l'événement devicemotion
+          }
     },
 
     ready(){
@@ -24,9 +29,19 @@ const App = {
     initComponents(){
 
 
-    }
+    },
+
+    process(event) {
+        var x = event.accelerationIncludingGravity.x;
+        var y = event.accelerationIncludingGravity.y;
+        var z = event.accelerationIncludingGravity.z;
+        document.getElementById("log").innerHTML = "<ul><li>X : " + x + "</li><li>Y : " + y + "</li><li>Z : " + z + "</li></ul>";
+      }
 
 };
+
+
+
 
 
 App.init();
