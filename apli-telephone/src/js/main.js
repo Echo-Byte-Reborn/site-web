e94ece7c1b75146e42df4f911b2f5ea238f64d66
Riddsaw <axlds98@gmail.com>
2018-12-07 00:17:38 +0100

    modif main.js

--- apli-telephone/src/js/main.js
+++ apli-telephone/src/js/main.js
@@ -1,10 +1,11 @@
 
-import Example from './components/Example'
 import News from './components/News'
 import {getBrowser} from './utils/environment'
+import Acceleration from './components/Acceleration'
 
 
 const App = {
+    
 
     init(){
         document.addEventListener('DOMContentLoaded', this.ready.bind(this), false);
@@ -15,18 +16,26 @@ const App = {
         this.bindEvent();
         console.log(getBrowser());
         new Example();
+        new Acceleration ();
         new News();
     },
 
     bindEvent(){
 
     },
+    
 
     initComponents(){
 
 
     },
 
+    process(event) {
+        var x = event.accelerationIncludingGravity.x;
+        var y = event.accelerationIncludingGravity.y;
+        var z = event.accelerationIncludingGravity.z;
+        document.getElementById("log").innerHTML = "<ul><li>X : " + x + "</li><li>Y : " + y + "</li><li>Z : " + z + "</li></ul>";
+      }
 
 };
 
