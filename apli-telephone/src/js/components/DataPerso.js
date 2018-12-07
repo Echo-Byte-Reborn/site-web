74bcfed11ef8e3c975370f44cc35e3dbd230b0eb
Messe Boris <essem0409@gmail.com>
2018-12-07 01:52:10 +0100

    cardiogramme

--- apli-telephone/src/js/components/DataPerso.js
+++ apli-telephone/src/js/components/DataPerso.js
@@ -1,11 +1,12 @@
 import $ from 'jquery';
 var verifMort=false;
+var tempsBPM =0;
 class DataPerso{
     constructor(){
         this.$bpm = document.querySelector(".bpm");
         setInterval(function(){
-            console.log(Math.floor((Math.random() * 60) + 60));
-            document.querySelector(".bpm").innerHTML=Math.floor((Math.random() * 60) + 60);
+            tempsBPM=Math.floor((Math.random() * 60) + 60)
+            document.querySelector(".bpm").innerHTML=tempsBPM;
         },1000);
         this.bindEvents();
         this.init();
